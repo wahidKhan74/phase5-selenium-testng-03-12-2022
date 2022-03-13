4a6f654d29b2ec2c6873a73b8e4634a3de6ecbe2
Wahid <wahid.khan74@gmail.com>
2022-03-13 23:23:06 +0530

    Update AmazonNavigationLinkTest.java

--- src/test/java/com/simplilearn/webapptest/amazon/AmazonNavigationLinkTest.java
+++ src/test/java/com/simplilearn/webapptest/amazon/AmazonNavigationLinkTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
