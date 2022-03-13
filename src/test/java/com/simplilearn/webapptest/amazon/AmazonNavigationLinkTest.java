a581a94b23c8c86fabd4831db7e8bca3c21bc7b6
Wahid <wahid.khan74@gmail.com>
2022-03-13 23:20:27 +0530

    Update AmazonNavigationLinkTest.java

--- src/test/java/com/simplilearn/webapptest/amazon/AmazonNavigationLinkTest.java
+++ src/test/java/com/simplilearn/webapptest/amazon/AmazonNavigationLinkTest.java
@@ -15,7 +15,7 @@ public class AmazonNavigationLinkTest {
 
 	// step 1: formulate a test url and driver path
 		String siteUrl = "https://www.amazon.in/";
-		String driverPath = "drivers/chromedriver";
+		String driverPath = "drivers/geckodriver";
 		WebDriver driver;
 		
 		@BeforeMethod
