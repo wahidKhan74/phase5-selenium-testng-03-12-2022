447b7470ad06784b05a3b8cb4fdb5db78e0f9662
Wahid Khan <wahid.khan74@gmail.com>
2022-03-13 15:27:19 +0000

    added hard and soft assert with reportng testreport plugin

---
+++ src/test/java/com/simplilearn/webapptest/TestNgAssertionDemo.java
@@ -0,0 +1,34 @@
+package com.simplilearn.webapptest;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class TestNgAssertionDemo {
+
+	// Hard assert : Hard Assert throws an AssertException immediately when an
+	// assert statement fails and test suite continues with next @Test method
+	@Test(description="Hard Assert method Test")
+	public void hardAssetionTest() {		
+		System.out.println("--- Hard assert method is started ---");
+		assertFalse(true);   // Assertion Error.
+		assertEquals(true, true);
+		assertTrue(true);
+		System.out.println("--- Hard assert method is completed ---");
+	}
+	
+	// Soft assert :- Soft Assert collects errors during @Test.
+	// Soft Assert does not throw an exception when an assert fails
+	@Test(description="Soft Assert method Test")
+	public void softAssertTest() {
+		SoftAssert softAssert = new SoftAssert();
+		System.out.println("--- Soft assert method is started ---");
+		softAssert.assertEquals("Hi", "Hello");
+		softAssert.assertEquals(true, false);
+		softAssert.assertTrue(true);
+		System.out.println("--- Soft assert method is completed ---");
+	}
+}
